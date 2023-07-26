7dd8c8a9f363bb2663ba9bc1eaec1ccddf1b592c
MSWSal <sahansalgado10@gmail.com>
2023-07-26 16:38:34 +0530

    amigoscodeJWT

--- src/main/java/com/postgresql/pgms/config/ApplicationConfig.java
+++ src/main/java/com/postgresql/pgms/config/ApplicationConfig.java
@@ -21,7 +21,7 @@ public class ApplicationConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-        return (UserDetailsService) username -> repository.findByEmail(username)
+        return username -> repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
     }
 
