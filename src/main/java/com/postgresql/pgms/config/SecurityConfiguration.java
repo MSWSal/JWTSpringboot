4e5cdbd14ce2064bcbb0aaa63eedb509e0c91f63
MSWSal <sahansalgado10@gmail.com>
2023-07-28 08:47:39 +0530

    ***without logout handler

--- src/main/java/com/postgresql/pgms/config/SecurityConfiguration.java
+++ src/main/java/com/postgresql/pgms/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -32,7 +33,18 @@ public class SecurityConfiguration {
                 )
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(c ->c
+                                .logoutUrl("/api/v1/auth/logout")
+                                .addLogoutHandler(null)
+                                .logoutSuccessHandler((request, response, authentication) ->
+                                        SecurityContextHolder.clearContext()
+                                )
+
+                        )
+
+        ;
+
 
 
 
